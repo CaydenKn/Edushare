6de632407974f94b6ee65bd1b0d3f067c505d703
Cayden Knight <83384718+CaydenKn@users.noreply.github.com>
2024-09-16 14:37:21 -0500

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://vmsjwnvshfackwldcfbq.supabase.co'
+const supabaseUrl = process.env.NEXT_PUBLIC_URL
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
